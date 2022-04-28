d141b95667e374693ab310c91a616b364821cce8
Haaroon Yousaf <Haaroon@users.noreply.github.com>
2022-04-28 11:43:25 +0100

    testing new pypi

---
+++ python/raphtoryclient/__init__.py
@@ -0,0 +1,8 @@
+"""
+Raphtory-client
+========
+This is the python client for the Raphtory project.
+Maintainer: Haaroon Yousaf, Pometry LTD
+"""
+from raphtoryclient import client
+from raphtoryclient.client import *
